45d9eb5683087f1efabcd86a68e88306828d7a85
antoinemeyer5 <antoine.meyer54@gmail.com>
2022-02-10 21:04:09 +0100

    Commentaires

--- Sources/PremierNiveau.c
+++ Sources/PremierNiveau.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//fonction qui prend un string et un nombre de caractère à afficher et les affichent
+// sera utile pour annee et semaine mais surtout pour le nom qui ne doit pas avoir de \0 à la fin
 
 int AllouerPremierNiveau(PremierNiveau_t ** TeteListe)
 {
